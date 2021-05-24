94b5fd682c3a82cedac15264c1695de0e094a3cd
Dusan Ilic <dusan.ilic@novelic.com>
2021-05-24 12:39:45 +0200

    Changed colorscheme

--- config.def.h
+++ config.def.h
@@ -8,9 +8,9 @@ static const char *fonts[] = {
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#cccccc", "#282a36" },
-	[SchemeSel]  = { "#ffffff", "#924441" },
+	/*               fg         bg       */
+	[SchemeNorm] = { "#bbbbbb", "#282828" },
+	[SchemeSel]  = { "#ffffff", "#7c7c7c" },
 	[SchemeOut]  = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
