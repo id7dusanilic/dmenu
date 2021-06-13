bb88ddfca85bb47c60d598b2c427c43f887b6cb7
Dusan Ilic <idtronicsrs@gmail.com>
2021-06-13 15:18:57 +0200

    config.h is used + bar padding option added

--- config.h
+++ config.h
@@ -9,12 +9,14 @@ static const char *fonts[] = {
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*               fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#282828" },
+	[SchemeNorm] = { "#bbbbbb", "#222222" },
 	[SchemeSel]  = { "#ffffff", "#7c7c7c" },
 	[SchemeOut]  = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
+
+static unsigned int barpadpx   = 6; /* Additional vertical padding (height) in pixels*/
 
 /*
  * Characters not considered part of a word while deleting words
